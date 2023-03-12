29bd5581acff06ed0b9edda2b5ac4fb4e5fe1fdb
GiannosL <giannos.louloudis@gmail.com>
2023-03-12 13:52:34 +0100

    wildcards

---
+++ scripts/example_j/script_b.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+multiplier_value = snakemake.params.multiplier
+
+df = pd.read_csv(snakemake.input.n, sep=",")
+df["column-A"] = df["column-A"]*multiplier_value
+df["column-B"] = df["column-B"]*multiplier_value
+df["column-C"] = df["column-C"]*multiplier_value
+
+df.to_csv(snakemake.output.s, index=False, sep=",")
